f3bd168d85e12de1757ddd531e5561758efba07b
Denis Puzikov <puzanpuzani@gmail.com>
2022-10-31 13:33:05 +0400

    First Version

---
+++ src/main/java/ru/puzikov/springcourse/FirstSecurityApp/services/PersonDetailsService.java
@@ -0,0 +1,33 @@
+package ru.puzikov.springcourse.FirstSecurityApp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.puzikov.springcourse.FirstSecurityApp.Models.Person;
+import ru.puzikov.springcourse.FirstSecurityApp.repositories.PeopleRepository;
+import ru.puzikov.springcourse.FirstSecurityApp.security.PersonDetails;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailsService implements UserDetailsService {
+
+    private final PeopleRepository peopleRepository;
+
+    @Autowired
+    public PersonDetailsService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<Person> person = peopleRepository.findByUsername(s);
+
+        if (person.isEmpty())
+            throw new UsernameNotFoundException("User not found");
+
+        return new PersonDetails(person.get());
+    }
+}
